41f16507f2766854f4398e5756d23dd650cb6802
William Omø <williamjean.k.o@gmail.com>
2023-05-10 21:08:53 +0200

    app revorked

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import userRouter from "./routers/user-router.js";
 import accountRouter from "./routers/account-router.js";
 import transactionRouter from "./routers/transaction-router.js";
-//import amqp from 'amqplib/callback_api.js';
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 
@@ -34,25 +33,6 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-
-
-/*amqp.connect('amqp://localhost', (err, conn) => {
-    if(err) {
-        throw err;
-    }
-    conn.createChannel((err, ch) => {
-        if(err) {
-            throw err;
-        }
-        let queueName = 'servermessage';
-        ch.assertQueue(queueName, {durable: false});
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueName);
-        ch.consume(queueName, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-            ch.ack(msg);
-        });
-    });
-});*/
 
 const PORT = process.env.PORT ||  5050;
 app.listen(PORT, () => console.log("Server running on:", PORT));
