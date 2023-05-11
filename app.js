595e1a589de9ab023ed0b10b195c348ccbf08a35
William Omø <williamjean.k.o@gmail.com>
2023-05-11 22:55:18 +0200

    updated with basic rabbitmq

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import accountRouter from "./routers/account-router.js";
 import transactionRouter from "./routers/transaction-router.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
+import amqp from 'amqplib/callback_api.js';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -33,6 +37,40 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// RABBITMQ
+amqp.connect(process.env.CLOUDAMQP_URL + "?heartbeat=60", (err, connection) => {
+    let r = 0;
+    if (err) {
+        throw err;
+    }
+    connection.createChannel(function(error, channel) {
+    if (error) {
+        throw error;
+    }
+    let queue = 'customer_control_message';
+
+    channel.assertQueue(queue, {
+        durable: false
+    });
+    channel.prefetch(1);
+    console.log(' [x] Awaiting RPC requests');
+    channel.consume(queue, function reply(msg) {
+        console.log(" [x] Received 1/2 %s", msg.content.toString());
+        // DO DB STUFF
+        if(msg.content.toString() === "read-all") {
+            r = 10;
+        }else{
+            r = 11;
+        }
+        channel.sendToQueue(msg.properties.replyTo,Buffer.from(r.toString()), {
+            correlationId: msg.properties.correlationId
+        });
+        console.log(" [x] Received 2/2 %s", msg.content.toString());
+        channel.ack(msg);
+    });
+    });
+});
 
 const PORT = process.env.PORT ||  5050;
 app.listen(PORT, () => console.log("Server running on:", PORT));
