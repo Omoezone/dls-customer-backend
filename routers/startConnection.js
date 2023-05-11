443c143f8c62cd8bbc996de4fac83ed21f2caed0
William Omø <williamjean.k.o@gmail.com>
2023-05-11 14:48:40 +0200

    redirection conn

--- routers/startConnection.js
+++ routers/startConnection.js
@@ -4,7 +4,17 @@ dotenv.config();
 
 // Create connection to mysql database and import where its needed
 
-let pool = mysql.createPool(process.env.CONNECTION_STRING);
+let pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_AZURE_HOST,
+    user: process.env.DB_AZURE_USER,
+    password: process.env.DB_AZURE_PASSWORD,
+    database: process.env.DB_AZURE_DATABASE,
+    port: process.env.DB_AZURE_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 pool.getConnection((err,connection)=> {
     if(err)
