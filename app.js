58f01c46555a5f81ae9dfe4b88c35d58d00b0d5c
William Omø <williamjean.k.o@gmail.com>
2023-05-22 14:30:56 +0200

    cleaned app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import userRouter from "./routers/user-router.js";
 import {getCustomers, getSingleCustomer, getDeletedCustomers, updateCustomer, deleteCustomer, createCustomer} from "./routers/user-router.js";
 import accountRouter from "./routers/account-router.js";
@@ -6,7 +6,6 @@ import transactionRouter from "./routers/transaction-router.js";
 import { ServiceBusClient } from "@azure/service-bus";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
-import amqp from 'amqplib';
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -18,14 +17,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(accountRouter);
 app.use(transactionRouter);
-
-// The function calls for the routes
-//console.log("hello",await getCustomers());
-//console.log("hello single:",await getSingleCustomer({id: 1}));
-//console.log("hello deleted:",await getDeletedCustomers());
-//console.log("hello update:",await updateCustomer({id: 1, firstname: "test", lastname:"lastname", age:80, email:"email@mail.dk", password:"HelloAdgangskode"}));
-//console.log("hello delete:",await deleteCustomer({id: 1}));
-//console.log("hello create:",await createCustomer({firstname: "NyCus", lastname:"nyCustomerLast", age:80, email:"email@mail", password:"HelloAdgangskode"}));
 
 // Swagger Options, used for documentation and UI
 const options = {
@@ -47,85 +38,6 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-
-// RABBITMQ
-async function connectRabbit() {
-    try {
-    let connection = await amqp.connect(process.env.CLOUDAMQP_URL + "?heartbeat=60");
-    let channel = await connection.createChannel();
-    const queue = 'customer_control_message';
-
-    channel.assertQueue(queue, {durable: false});
-    channel.prefetch(1);
-    console.log(' [x] Awaiting RPC requests');
-    channel.consume(queue, function reply(msg) {
-        console.log(" [x] Received 1/2 %s", msg.content.toString());
-
-        // DO DB STUFF
-        if(msg.content.toString() === "read-all") {
-            console.log("Entered read-all")
-            getCustomers().then((result) => {
-                console.log(result[0])
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            });
-
-        }else if(msg.content.toString() === "read-single") {
-            console.log("Entered read-single")
-            getSingleCustomer({id: 1}).then((result) => {
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            }
-            );
-        }else if(msg.content.toString() === "read-deleted") {
-            console.log("Entered read-deleted")
-            getDeletedCustomers().then((result) => {
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            });
-        }else if(msg.content.toString() === "update") {
-            console.log("Entered update");
-            updateCustomer({id: 1, firstname: "test", lastname:"lastname", age:80, email:"@mail.dk", password:"HelloAdgangskode"}).then((result) => {
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            });
-        }else if(msg.content.toString() === "delete") {
-            console.log("Entered delete");
-            deleteCustomer({id: 1}).then((result) => {
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            });
-        }else if(msg.content.toString() === "create") {
-            console.log("Entered create");
-            createCustomer({firstname: "NyCus", lastname:"nyCustomerLast", age:80, email:"email@mail", password:"HelloAdgangskode"}).then((result) => {
-                channel.sendToQueue(msg.properties.replyTo,Buffer.from(result.toString()), {
-                    correlationId: msg.properties.correlationId
-                });
-                console.log(" [x] Received 2/2 %s", msg.content.toString());
-                channel.ack(msg);
-            });
-        }
-    });
-    } catch (error) {
-        console.log(error);
-    }
-};
-//connectRabbit();
 
 async function recieveFromQueue() {
     const connectionString = process.env.AZURE_SERVICE_BUS;
